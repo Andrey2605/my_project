5e8d53e2121d975338629bc925570c45ab01f052
Andrey <fatym98@gmail.com>
2024-08-11 12:53:10 +0400

    homework_10_1

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-list = [
+date = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
@@ -8,10 +8,10 @@ list = [
 state = "EXECUTED"
 
 
-def filter_by_state(state: str) -> str:
-    '''Функция возвращает новый список словарей, у которых ключ state соответствует указанному значению'''
+def filter_by_state(state: str) -> list:
+    """Функция возвращает новый список словарей, у которых ключ state соответствует указанному значению"""
     new_list = []
-    for dictonary in list:
+    for dictonary in date:
         if dictonary["state"] == state:
             new_list.append(dictonary)
     return new_list
@@ -20,10 +20,10 @@ def filter_by_state(state: str) -> str:
 print(filter_by_state(state))
 
 
-def sort_by_date(list: str)-> str:
+def sort_by_date(date: list) -> list:
     """Функция возвращает новый список, отсортированный по дате"""
-    sorted_list = sorted(list, key=lambda x: x["date"], reverse=True)
+    sorted_list = sorted(date, key=lambda x: x["date"], reverse=True)
     return sorted_list
 
 
-print(sort_by_date(list))
+print(sort_by_date(date))
