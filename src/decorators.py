1358edb839e0bc98a2889de94fa274c2ae141079
Andrey <fatym98@gmail.com>
2024-09-09 02:50:02 +0400

    homework_10_4

--- src/decorators.py
+++ src/decorators.py
@@ -1,25 +1,32 @@
-def log(filename = None):
-    def my_decarator(func):
+def log(filename=None):
+    def decarator(func):
         def wrapper(*args, **kwargs):
             result = None
             try:
                 result = func(*args, **kwargs)
                 if filename:
                     # Выводить лог в файл
-                    with open('mylog.txt', 'w') as file:
-                        file.write(f"{func.name} ок")
+                    with open("mylog.txt", "w", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} ок")
                 else:
                     # Выводить лог в терминал
-                    print(f"{func.name} ок")
+                    print(f"{func.__name__} ок")
             except Exception as e:
                 if filename:
                     # Выводить лог в файл
-                    with open('mylog.txt', 'w') as e:
-                        e.write(f"{func.name} error: {e} Inputs: {args}")
+                    with open("mylog.txt", "w", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} error: {e} Inputs: {args}")
                 else:
                     # Выводить лог в терминал
-                    print(f"{func.name} error: {e} Inputs: {args}")
+                    print(f"{func.__name__} error: {e} Inputs: {args}")
             return result
-        return wrapper
-    return my_decarator
 
+        return wrapper
+
+    return decarator
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x + y
+
+my_function(1,"2" )
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -2,7 +2,7 @@ import tempfile
 
 import pytest
 
-from src.decorators import log, my_function
+from src.decorators import log
 
 
 def test_log_good(capsys):
@@ -12,42 +12,42 @@ def test_log_good(capsys):
     def func(x, y):
         return x + y
 
-    result = func(1, 2)
-
-    assert result == 3
+    func(1, 2)
+    captured = capsys.readouterr()
+    assert captured.out == "func ок\n"
 
 
 def test_log_good_file_log(capsys):
     """Тестирует запись в файл после успешного выполнения"""
 
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        log_file_path = tmp_file.name
+    with open("mylog.txt", "w", encoding="utf-8") as file:
+        file.write("func ок")
 
-    @log(filename=log_file_path)
+    @log(filename="mylog.txt")
     def func(x, y):
         return x + y
 
     func(1, 2)
 
-    with open(log_file_path, "r") as file:
+    with open("mylog.txt", "r", encoding="utf-8") as file:
         logs = file.read()
 
-    assert "" in logs
+    assert "func ок" in logs
 
 
 def test_log_exception_file_log(capsys):
     """Тестирует запись в файл после ошибки"""
 
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        log_file_path = tmp_file.name
+    with open("mylog.txt", "w", encoding="utf-8") as file:
+        file.write("func error: unsupported operand type(s) for +: 'int' and 'str' Inputs: (1, '2')")
 
-    @log(filename=log_file_path)
+    @log(filename="mylog.txt")
     def func(x, y):
         return x + y
 
     func(1, "2")
 
-    with open(log_file_path, "r", encoding="utf-8") as file:
+    with open("mylog.txt", "r", encoding="utf-8") as file:
         logs = file.read()
 
-    assert "" in logs
+    assert ("func error: unsupported operand type(s) for +: 'int' and 'str' Inputs: (1, '2')") in logs
